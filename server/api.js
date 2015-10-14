37813e2eee2175dab0c4e6f4e9544ab40fd3a795
leozdgao <gzdleo0816@gmail.com>
2015-10-15 00:16:17 +0800

    add login page

--- server/api.js
+++ server/api.js
@@ -1,13 +1,16 @@
 var router = require('express').Router()
+var bodyParser = require('body-parser')
+
+router.use(bodyParser.json())
 
 router.post('/task', function (req, res) {
-  setTimeout(() => {
+  setTimeout(function () {
     res.json({ success: true })
   }, 2000)
 })
 
 router.post('/check', function (req, res) {
-  setTimeout(() => {
+  setTimeout(function () {
     res.json({ isValid: req.body.user !== 'leozdgao' })
   }, 1000)
 })
--- server/index.js
+++ server/index.js
@@ -1,24 +1,35 @@
 var path = require('path')
 var app = require('express')()
 var server = require('http').createServer(app)
-var bodyParser = require('body-parser')
 
-var webpack = require('webpack')
-var config = require('../webpack.config')
-var compiler = webpack(config)
+var __IS_DEV__ = process.env['NODE_ENV'] === 'production'
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath,
-  stats: {
-    colors: true
-  }
-}))
+if (__IS_DEV__) {
 
-app.use(require('webpack-hot-middleware')(compiler))
+  var webpack = require('webpack')
+  var config = require('../webpack.config')
+  var compiler = webpack(config)
 
-app.use(bodyParser.json())
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath,
+    stats: {
+      colors: true
+    }
+  }))
+
+  app.use(require('webpack-hot-middleware')(compiler))
+}
+
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+// mock api
 app.use('/api', require('./api'))
+
+app.get('/login', function (req, res) {
+  res.render('login')
+})
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'))
